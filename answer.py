5efc31869e32ba906786c88a1efbaada43f0617c
usatyo <yu0424.math8128@gmail.com>
2024-06-11 13:44:38 +0900

    add description

--- answer.py
+++ answer.py
@@ -4,6 +4,10 @@
 
 
 # 内積
+from collections import deque
+from math import atan2
+
+
 def dot(v, w):
     return v[0] * w[0] + v[1] * w[1]
 
@@ -137,19 +141,208 @@ def is_inside_polygon(ps, q):
         return -1
 
 
-n = int(input())
-ps = []
-for _ in range(n):
-    x, y = map(int, input().split())
-    ps.append([x, y])
+# 凸包
+def convex_hull(ps):
+    """凸包
 
-q = int(input())
+    Args:
+        points (float[][]): 点のリスト、[x, y, ...]
 
-for _ in range(q):
-    x, y = map(int, input().split())
-    if is_inside_polygon(ps, [x, y]) == 0:
-        print(1)
-    elif is_inside_polygon(ps, [x, y]) == 1:
-        print(2)
-    else:
-        print(0)
+    Returns:
+        float[][]: 凸包を構成する頂点を下側から順に反時計回りに返す
+    """
+    n = len(ps)
+    ps.sort(key=lambda x: (x[1], x[0]))
+
+    if n <= 2:
+        return ps
+    elif n == 3:
+        if counter_clockwise(ps[0], ps[1], ps[2]) == 1:
+            return ps
+        else:
+            return [ps[0], ps[2], ps[1]]
+
+    right = deque([ps[0][:], ps[1][:]])
+    for i in range(2, n):
+        next = ps[i][:]
+        curr = right.pop()
+        prev = right.pop()
+        while True:
+            if counter_clockwise(prev, curr, next) >= 0:
+                right.append(prev)
+                right.append(curr)
+                break
+            if len(right) == 0:
+                right.append(prev)
+                break
+            curr = prev[:]
+            prev = right.pop()
+
+        right.append(next)
+
+    left = deque([ps[n - 1][:], ps[n - 2][:]])
+    for i in range(n - 2)[::-1]:
+        next = ps[i][:]
+        curr = left.pop()
+        prev = left.pop()
+        while True:
+            if counter_clockwise(prev, curr, next) >= 0:
+                left.append(prev)
+                left.append(curr)
+                break
+            if len(left) == 0:
+                left.append(prev)
+                break
+            curr = prev[:]
+            prev = left.pop()
+
+        left.append(next)
+
+    right.pop()
+    left.pop()
+    return list(right) + list(left)
+
+
+# 凸多角形の直径
+def diameter_convex_polygon(ps):
+    ch = convex_hull(ps)
+    n = len(ch)
+    if n == 2:
+        return distance(ch[0], ch[1])
+    i = j = 0
+    for k in range(n):
+        if ch[k][0] < ch[i][0]:
+            i = k
+        if ch[k][0] > ch[j][0]:
+            j = k
+    res = 0
+    si, sj = i, j
+    while i != sj or j != si:
+        res = max(res, distance(ch[i], ch[j]))
+        if (
+            cross(
+                [ch[(i + 1) % n][0] - ch[i][0], ch[(i + 1) % n][1] - ch[i][1]],
+                [ch[(j + 1) % n][0] - ch[j][0], ch[(j + 1) % n][1] - ch[j][1]],
+            )
+            < 0
+        ):
+            i = (i + 1) % n
+        else:
+            j = (j + 1) % n
+
+    return res
+
+
+# 三角形の内心
+def incenter(p0, p1, p2):
+    a = distance(p1, p2)
+    b = distance(p2, p0)
+    c = distance(p0, p1)
+    x = (a * p0[0] + b * p1[0] + c * p2[0]) / (a + b + c)
+    y = (a * p0[1] + b * p1[1] + c * p2[1]) / (a + b + c)
+    r = area_polygon([p0, p1, p2]) * 2 / (a + b + c)
+    return x, y, r
+
+
+# 三角形の外心
+def curcumcenter(p1, p2, p3):
+    a = 2 * (p1[0] - p2[0])
+    b = 2 * (p1[1] - p2[1])
+    p = p1[0] ** 2 - p2[0] ** 2 + p1[1] ** 2 - p2[1] ** 2
+    c = 2 * (p1[0] - p3[0])
+    d = 2 * (p1[1] - p3[1])
+    q = p1[0] ** 2 - p3[0] ** 2 + p1[1] ** 2 - p3[1] ** 2
+    det = a * d - b * c
+    x = d * p - b * q
+    y = a * q - c * p
+    if det < 0:
+        x = -x
+        y = -y
+        det = -det
+    x /= det
+    y /= det
+    r = ((x - p1[0]) ** 2 + (y - p1[1]) ** 2) ** 0.5
+    return x, y, r
+
+
+while True:
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
+    circles = []
+    for _ in range(n):
+        x, y, r = map(int, input().split())
+        circles.append([x, y, r])
+
+    neighbors = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            if (
+                abs(circles[i][2] - circles[j][2])
+                < distance(circles[i][:2], circles[j][:2])
+                < circles[i][2] + circles[j][2]
+            ):
+                neighbors[i].append(j)
+
+    for i in range(n):
+        neighbors[i].sort(
+            key=lambda x: atan2(
+                circles[x][1] - circles[i][1], circles[x][0] - circles[i][0]
+            )
+        )
+
+    for _ in range(n):
+        for i in range(n):
+            if len(neighbors[i]) == 1:
+                neighbors[neighbors[i][0]].remove(i)
+                neighbors[i] = []
+
+    polygons = []
+    for i in range(n):
+        for j in range(n):
+            if neighbors[i].count(j) == 0:
+                continue
+            l = [i, j]
+            while True:
+                curr = l.pop()
+                prev = l.pop()
+                next = neighbors[curr][
+                    (neighbors[curr].index(prev) + 1) % len(neighbors[curr])
+                ]
+                l.append(prev)
+                l.append(curr)
+                if next == prev:
+                    break
+                if next == i:
+                    polygons.append([circles[p] for p in l])
+                    break
+                l.append(next)
+
+    answers = []
+    for _ in range(m):
+        px, py, qx, qy = map(int, input().split())
+        # draw.ellipse((px + 900, py + 900, px + 1100, py + 1100), fill=(255, 0, 0))
+        # draw.ellipse((qx + 900, qy + 900, qx + 1100, qy + 1100), fill=(255, 0, 0))
+        ans = "YES"
+        flag = []
+        for x, y, r in circles:
+            if (distance([px, py], [x, y]) < r) ^ (distance([qx, qy], [x, y]) < r):
+                ans = "NO"
+            flag.append(distance([px, py], [x, y]) < r)
+
+        if ans == "NO":
+            answers.append(ans)
+            continue
+        if any(flag):
+            answers.append(ans)
+            continue
+        for ps in polygons:
+            if is_inside_polygon(ps, [px, py]) * is_inside_polygon(ps, [qx, qy]) == -1:
+                ans = "NO"
+                break
+
+        answers.append(ans)
+
+    print(*answers)
--- geometry.py
+++ geometry.py
@@ -1,37 +1,89 @@
 # 出力が指数形式にならないよう注意
 # print(f"{num:.10f}") などとする
 # 適当な大きさの素数 99961, 99971, 99989, 99991
+# matplotlib, pillow などの図形描画ライブラリがあるといいかも
+# from PIL import Image, ImageDraw
+# im = Image.new('RGB', (500, 300), (255, 255, 255))
+# draw = ImageDraw.Draw(im)
+# draw.ellipse((100, 100, 200, 200), fill=None, outline=(0, 0, 0))
+# draw.rectangle((200, 100, 300, 200), fill=(0, 192, 192), outline=(255, 255, 255))
+# draw.line((350, 200, 450, 100), fill=(255, 255, 0), width=10)
+# im.save('./pillow_image.jpg', quality=95)
 
 
-# 内積
+from collections import deque
+
+
 def dot(v, w):
+    """内積
+
+    Args:
+        v (list): ベクトル1
+        w (list): ベクトル2
+
+    Returns:
+        number: v と w の内積
+    """
     return v[0] * w[0] + v[1] * w[1]
 
 
-# 外積の係数(= vを90度回転させた内積)
 def cross(v, w, o=[0, 0]):
+    """外積の係数(= vを90度回転させた内積)
+
+    Args:
+        v (list): ベクトル1
+        w (list): ベクトル2
+        o (list, optional): 原点. Defaults to [0, 0].
+
+    Returns:
+        number: o を原点とした v と w の外積の係数
+    """
     v = [v[0] - o[0], v[1] - o[1]]
     w = [w[0] - o[0], w[1] - o[1]]
     return v[0] * w[1] - v[1] * w[0]
 
 
-# v に対する w の射影
 def project(v, w, o=[0, 0]):
+    """射影
+
+    Args:
+        v (list): 射影先のベクトル
+        w (list): 射影元のベクトル
+        o (list, optional): 原点. Defaults to [0, 0].
+
+    Returns:
+        list: v に対する w の射影
+    """
     v = [v[0] - o[0], v[1] - o[1]]
     w = [w[0] - o[0], w[1] - o[1]]
     return [o[0] + v[0] * dot(v, w) / dot(v, v), o[1] + v[1] * dot(v, w) / dot(v, v)]
 
 
-# v に対する w の反射(対称な位置にある点)
 def reflect(v, w):
+    """反射
+
+    Args:
+        v (list): 軸となるベクトル
+        w (list): 移動する点
+
+    Returns:
+        list: v に対する w の反射
+    """
     p = project(v, w)
     return [2 * p[0] - w[0], 2 * p[1] - w[1]]
 
 
-# v を基準とした w の回転方向判定
-# 反時計回り:1, 時計回り:-1, 直線上:0
-# 直線上の判定は整数の場合のみ可能
 def counter_clockwise(v, w, o=[0, 0]):
+    """v を基準とした w の回転方向判定
+
+    Args:
+        v (list): ベクトル1
+        w (list): ベクトル2
+        o (list, optional): 原点. Defaults to [0, 0].
+
+    Returns:
+        number: 反時計回り:1, 時計回り:-1, 直線上:0
+    """
     v = [v[0] - o[0], v[1] - o[1]]
     w = [w[0] - o[0], w[1] - o[1]]
     if cross(v, w) > 0:
@@ -45,6 +97,16 @@ def counter_clockwise(v, w, o=[0, 0]):
 # w が v の線分上にあるか判定
 # 整数のみ使用可能
 def on_segment(v, w, o=[0, 0]):
+    """線分上の判定
+
+    Args:
+        v (list): 線分を表すベクトル
+        w (list): 判定する点
+        o (list, optional): 原点. Defaults to [0, 0].
+
+    Returns:
+        bool: w が v の線分上にある場合 True
+    """
     v = [v[0] - o[0], v[1] - o[1]]
     w = [w[0] - o[0], w[1] - o[1]]
     if counter_clockwise(v, w) != 0:
@@ -52,8 +114,18 @@ def on_segment(v, w, o=[0, 0]):
     return 0 <= dot(v, w) <= dot(v, v)
 
 
-# 線分 p0-p1 と q0-q1 の交差判定
 def intersect_segment(p0, p1, q0, q1):
+    """線分の交差判定
+
+    Args:
+        p0 (list): 線分1の始点
+        p1 (list): 線分1の終点
+        q0 (list): 線分2の始点
+        q1 (list): 線分2の終点
+
+    Returns:
+        bool: 線分1と線分2が交差している場合 True
+    """
     return (
         counter_clockwise(p0, q1, q0) * counter_clockwise(q1, p1, q0) > 0
         and counter_clockwise(q0, p1, p0) * counter_clockwise(p1, q1, p0) > 0
@@ -64,21 +136,48 @@ def intersect_segment(p0, p1, q0, q1):
     )
 
 
-# 線分 p0-p1 と q0-q1 の交点の座標
 def intersection_point(p0, p1, q0, q1):
+    """線分の交点
+
+    Args:
+        p0 (list): 線分1の始点
+        p1 (list): 線分1の終点
+        q0 (list): 線分2の始点
+        q1 (list): 線分2の終点
+
+    Returns:
+        list: 交点の座標
+    """
     t = cross(q0, p1, p0) / cross(
         [p1[0] - p0[0], p1[1] - p0[1]], [q1[0] - q0[0], q1[1] - q0[1]]
     )
     return [q0[0] + t * (q1[0] - q0[0]), q0[1] + t * (q1[1] - q0[1])]
 
 
-# 点 p と点 q の距離
 def distance(p, q):
+    """２点間の距離
+
+    Args:
+        p (list): 点1
+        q (list): 点2
+
+    Returns:
+        number: 点1 と 点2 の距離
+    """
     return ((p[0] - q[0]) ** 2 + (p[1] - q[1]) ** 2) ** 0.5
 
 
-# 点 p と線分 q0-q1 の距離
 def distance_point_segment(p, q0, q1):
+    """点と線分の距離
+
+    Args:
+        p (list): 点の座標
+        q0 (list): 線分の端点1
+        q1 (list): 線分の端点2
+
+    Returns:
+        number: 点と線分の距離
+    """
     pj = project(q1, p, q0)
     if distance(pj, q0) < distance(q0, q1) and distance(pj, q1) < distance(q0, q1):
         return abs(cross(q1, p, q0) / distance(q0, q1))
@@ -89,8 +188,18 @@ def distance_point_segment(p, q0, q1):
         )
 
 
-# 線分 p0-p1 と q0-q1 の距離
 def distance_segment(p0, p1, q0, q1):
+    """線分と線分の距離
+
+    Args:
+        p0 (list): 線分1の始点
+        p1 (list): 線分1の終点
+        q0 (list): 線分2の始点
+        q1 (list): 線分2の終点
+
+    Returns:
+        number: 線分1と線分2の距離
+    """
     if intersect_segment(p0, p1, q0, q1):
         return 0
     return min(
@@ -101,8 +210,15 @@ def distance_segment(p0, p1, q0, q1):
     )
 
 
-# 多角形の面積
 def area_polygon(ps):
+    """多角形の面積(凹凸、回転方向問わず)
+
+    Args:
+        ps (list<list>): 多角形の頂点のリスト
+
+    Returns:
+        number: 面積
+    """
     n = len(ps)
     s = 0
     for i in range(n):
@@ -110,8 +226,15 @@ def area_polygon(ps):
     return abs(s) / 2
 
 
-# 凸多角形判定
 def is_convex_polygon(ps):
+    """凸多角形判定
+
+    Args:
+        ps (list<list>): 多角形の頂点のリスト
+
+    Returns:
+        bool: 凸多角形であれば True
+    """
     n = len(ps)
     l = []
     for i in range(n):
@@ -119,9 +242,16 @@ def is_convex_polygon(ps):
     return min(l) >= 0 or max(l) <= 0
 
 
-# 多角形の点の内外判定
-# 内部:1, 外部:-1, 辺上:0
 def is_inside_polygon(ps, q):
+    """多角形と点の関係
+
+    Args:
+        ps (list<list>): 多角形の頂点のリスト
+        q (list): 点の座標
+
+    Returns:
+        number: 内部:1, 外部:-1, 辺上:0
+    """
     n = len(ps)
     q0 = [q[0] + 99989, q[1] + 99991]
     count = 0
@@ -135,3 +265,151 @@ def is_inside_polygon(ps, q):
         return 1
     else:
         return -1
+
+
+def convex_hull(ps):
+    """凸包
+
+    Args:
+        points (list<list>): 多角形の頂点のリスト
+
+    Returns:
+        list<list>: 凸包を構成する頂点(下側から順に反時計回り)
+    """
+    n = len(ps)
+    ps.sort(key=lambda x: (x[1], x[0]))
+
+    if n <= 2:
+        return ps
+    elif n == 3:
+        if counter_clockwise(ps[0], ps[1], ps[2]) == 1:
+            return ps
+        else:
+            return [ps[0], ps[2], ps[1]]
+
+    right = deque([ps[0][:], ps[1][:]])
+    for i in range(2, n):
+        next = ps[i][:]
+        curr = right.pop()
+        prev = right.pop()
+        while True:
+            if counter_clockwise(prev, curr, next) >= 0:
+                right.append(prev)
+                right.append(curr)
+                break
+            if len(right) == 0:
+                right.append(prev)
+                break
+            curr = prev[:]
+            prev = right.pop()
+
+        right.append(next)
+
+    left = deque([ps[n - 1][:], ps[n - 2][:]])
+    for i in range(n - 2)[::-1]:
+        next = ps[i][:]
+        curr = left.pop()
+        prev = left.pop()
+        while True:
+            if counter_clockwise(prev, curr, next) >= 0:
+                left.append(prev)
+                left.append(curr)
+                break
+            if len(left) == 0:
+                left.append(prev)
+                break
+            curr = prev[:]
+            prev = left.pop()
+
+        left.append(next)
+
+    right.pop()
+    left.pop()
+    return list(right) + list(left)
+
+
+def diameter_convex_polygon(ps):
+    """凸多角形の直径(凸包を求めてから計算)
+
+    Args:
+        ps (list<list>): 凸多角形の頂点のリスト
+
+    Returns:
+        number: 最も遠い2点間の距離
+    """
+    ch = convex_hull(ps)
+    n = len(ch)
+    if n == 2:
+        return distance(ch[0], ch[1])
+    i = j = 0
+    for k in range(n):
+        if ch[k][0] < ch[i][0]:
+            i = k
+        if ch[k][0] > ch[j][0]:
+            j = k
+    res = 0
+    si, sj = i, j
+    while i != sj or j != si:
+        res = max(res, distance(ch[i], ch[j]))
+        if (
+            cross(
+                [ch[(i + 1) % n][0] - ch[i][0], ch[(i + 1) % n][1] - ch[i][1]],
+                [ch[(j + 1) % n][0] - ch[j][0], ch[(j + 1) % n][1] - ch[j][1]],
+            )
+            < 0
+        ):
+            i = (i + 1) % n
+        else:
+            j = (j + 1) % n
+
+    return res
+
+
+def incenter(p0, p1, p2):
+    """三角形の内心
+
+    Args:
+        p0 (list): 頂点1
+        p1 (list): 頂点2
+        p2 (list): 頂点3
+
+    Returns:
+        list: 内心のx座標、内心のy座標、内接円の半径を順に返す
+    """
+    a = distance(p1, p2)
+    b = distance(p2, p0)
+    c = distance(p0, p1)
+    x = (a * p0[0] + b * p1[0] + c * p2[0]) / (a + b + c)
+    y = (a * p0[1] + b * p1[1] + c * p2[1]) / (a + b + c)
+    r = area_polygon([p0, p1, p2]) * 2 / (a + b + c)
+    return x, y, r
+
+
+def curcumcenter(p1, p2, p3):
+    """三角形の外心
+
+    Args:
+        p1 (list): 頂点1
+        p2 (list): 頂点2
+        p3 (list): 頂点3
+
+    Returns:
+        list: 外心のx座標、外心のy座標、外接円の半径を順に返す
+    """
+    a = 2 * (p1[0] - p2[0])
+    b = 2 * (p1[1] - p2[1])
+    p = p1[0] ** 2 - p2[0] ** 2 + p1[1] ** 2 - p2[1] ** 2
+    c = 2 * (p1[0] - p3[0])
+    d = 2 * (p1[1] - p3[1])
+    q = p1[0] ** 2 - p3[0] ** 2 + p1[1] ** 2 - p3[1] ** 2
+    det = a * d - b * c
+    x = d * p - b * q
+    y = a * q - c * p
+    if det < 0:
+        x = -x
+        y = -y
+        det = -det
+    x /= det
+    y /= det
+    r = ((x - p1[0]) ** 2 + (y - p1[1]) ** 2) ** 0.5
+    return x, y, r
