4a55dc70c4c535c69141959a8f978f24692cbe51
usatyo <yu0424.math8128@gmail.com>
2025-01-25 06:47:23 +0900

    fix randomtest

--- randomtest.py
+++ randomtest.py
@@ -1,9 +1,8 @@
-from answer import check, main
+from answer import main
 from honesty import honesty
 from generator import Generator
 import unittest
 from tqdm import tqdm
-
 
 LOOP = 100
 
@@ -18,7 +17,7 @@ class Test(unittest.TestCase):
         honest = honesty()
         self.assertEqual(ans, honest, msg=f"\nYour Value: {ans}\nTrue Value: {honest}")
 
-    def _test_multiple_cases(self):
+    def test_multiple_cases(self):
         gen = Generator()
         for _ in tqdm(range(LOOP)):
             info = gen.generate()
@@ -30,23 +29,6 @@ class Test(unittest.TestCase):
                 self.assertEqual(
                     ans, honest, msg=f"\nYour Value: {ans}\nTrue Value: {honest}"
                 )
-
-    def test_satisfy_conditions(self):
-        gen = Generator()
-        for _ in range(LOOP):
-            info = gen.generate()
-            file = open("input.txt", "r")
-            ans = int(main(lambda: file.readline().rstrip()))
-            file.close()
-
-            file = open("input.txt", "r")
-            n = int(file.readline().rstrip())
-            file.close()
-
-            with self.subTest(info=info):
-                self.assertTrue(n <= ans < n * 2, "range error")
-                self.assertTrue(check(ans))
-                self.assertTrue(check(ans + 1))
 
 
 if __name__ == "__main__":
